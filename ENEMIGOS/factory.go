58365bce6a4c54306ce8f76a7e651bf3ed5a32d5
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-12-30 14:28:12 -0300

    Se crean enemigos desde los dos lados.-

---
+++ ENEMIGOS/factory.go
@@ -0,0 +1,30 @@
+components {
+  id: "enemigos_f"
+  component: "/ENEMIGOS/enemigos_f.factory"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "factory_enemigos"
+  component: "/ENEMIGOS/factory_enemigos.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ main/datos.lua
@@ -0,0 +1,8 @@
+local M = {}
+pantalla_h = 0;
+pantalla_w = 0;
+player_pos = vmath.vector3();
+
+
+
+return M;
