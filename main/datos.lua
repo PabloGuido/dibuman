72e88ba62e8108d48c1de91a36f185c673de53c6
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-01-01 23:13:41 -0300

    Avanzando con el sistema de vidas y game over.

--- main/datos.lua
+++ main/datos.lua
@@ -2,7 +2,7 @@ local M = {}
 M.pantalla_h = 0;
 M.pantalla_w = 0;
 M.player_pos = vmath.vector3();
-M.game_over = true;
+M.game_over = false;
 
 M.empezar_juego = function()
 	msg.post("main:/player/player", "empezar_juego")
