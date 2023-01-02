f0bffa1ab4636f9b8645602dc40dff0f95f2ace1
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-01-01 22:04:57 -0300

    Intro screen, boton empezar.

--- main/datos.lua
+++ main/datos.lua
@@ -1,8 +1,12 @@
 local M = {}
-pantalla_h = 0;
-pantalla_w = 0;
-player_pos = vmath.vector3();
+M.pantalla_h = 0;
+M.pantalla_w = 0;
+M.player_pos = vmath.vector3();
+M.game_over = true;
 
+M.empezar_juego = function()
+	msg.post("main:/player/player", "empezar_juego")
+end
 
 
 return M;
