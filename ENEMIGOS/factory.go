1b8278ab0695943e9f57e3c0cf6bbb2824375e60
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-01-04 16:00:14 -0300

    Naranjas agregadas. Falta ajustar detalles y ver cada cuanto se crean.

--- ENEMIGOS/factory.go
+++ ENEMIGOS/factory.go
@@ -15,7 +15,22 @@ components {
 }
 components {
   id: "enemigos_cf"
-  component: "/ENEMIGOS/enemigos_cf.collectionfactory"
+  component: "/ENEMIGOS/FRANCES/enemigos_cf.collectionfactory"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "NARANJA"
+  component: "/ENEMIGOS/NARANJA/NARANJA.collectionfactory"
   position {
     x: 0.0
     y: 0.0
