444f6991a60a6c2a0cd0a7721d591f5c3114718e
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-01-06 20:32:04 -0300

    Mas avances.

--- main/datos.lua
+++ main/datos.lua
@@ -3,6 +3,7 @@ M.pantalla_h = 0;
 M.pantalla_w = 0;
 M.player_pos = vmath.vector3();
 M.game_over = false;
+M.eliminar_enemigos = false;
 
 M.empezar_juego = function()
 	msg.post("main:/player/player", "empezar_juego")
