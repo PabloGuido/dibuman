fd2127dfb82a4c5da7652644b95e9a67f03c014d
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-01-07 19:27:51 -0300

    Se va armando.

--- ENEMIGOS/NAPO/ataques/pelotas.go
+++ ENEMIGOS/NAPO/ataques/pelotas.go
@@ -17,7 +17,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/ENEMIGOS/enemigos.atlas\"\n"
-  "default_animation: \"green\"\n"
+  "default_animation: \"red\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
